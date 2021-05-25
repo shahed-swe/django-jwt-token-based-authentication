6f54cd89df73d53392563eddb536d7cdbb28fdcc
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-05-26 00:13:50 +0600

    new commit:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,24 @@ import Signup from './containers/Signup'
 import Login from './containers/Login'
 import ResetPassword from './containers/ResetPassword'
 import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
+import  {Provider} from 'react-redux'
+import store from './store'
 
 const App = () => (
-    <Router>
-        <Layout>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/signup" component={Signup}/>
-                <Route exact path="/activate/:uid/:token" component={Activate}/>
-                <Route exact path="/reset_password" component={ResetPassword}/>
-                <Route exact path="/password/reset/confirm/:uid/:token" component={ResetPasswordConfirm}/>
-            </Switch>
-        </Layout>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/signup" component={Signup}/>
+                    <Route exact path="/activate/:uid/:token" component={Activate}/>
+                    <Route exact path="/reset_password" component={ResetPassword}/>
+                    <Route exact path="/password/reset/confirm/:uid/:token" component={ResetPasswordConfirm}/>
+                </Switch>
+            </Layout>
+        </Router>
+    </Provider>
 );
 
 export default App;
--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Login = () => (
-    <div>
-        Login
-    </div>
-)
-export default Login;
+
+const Login = () => {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const {email, password} = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+        // login(email, password)
+    }
+    // if the user is authenticated we will redirect the user to home page
+
+    return (
+        <div class="container mt-5">
+            <h1>Sign in</h1>
+            <p>Sign in to your account</p>
+            <form onSubmit={e => onsubmit(e)}>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        className = 'from-control'
+                        type="email"
+                        placeholder="Enter your email"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        className="form-control"
+                        type="password"
+                        placeholder="Enter your password"
+                        name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
+
+                </div>
+                <button class="btn btn-primary" type="submit">Login</button>
+            </form>
+            <p className="mt-3">
+                Don't have an account? <Link to="/signup">Signup</Link>
+            </p>
+            <p className="mt-3">
+                Forgot password? <Link to="/reset_password">ResetPassword</Link>
+            </p>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({
+    // is authenticated ?
+})
+export default connect(null)(Login);
