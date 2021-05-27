33d59ad75b5f6323cc10b19489e665953995c9fa
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-05-27 23:08:47 +0600

    new commit

--- src/src/urls.py
+++ src/src/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
-# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
